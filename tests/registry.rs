3450c1eca119ae1520417d7c51d4d3959102005f
Lukas Woodtli <lukas.woodtli@husqvarnagroup.com>
2025-01-15 11:59:22 +0100

    Refactor test

--- tests/registry.rs
+++ tests/registry.rs
@@ -1,12 +1,16 @@
 use lwm2m_registry::Registry;
 use std::path::PathBuf;
 
-#[tokio::test]
-async fn test_load_registry() -> Result<(), Box<dyn std::error::Error>> {
+async fn load_test_registry() -> Result<Registry, Box<dyn std::error::Error>> {
     let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     d.push("tests/spec_files");
 
-    let registry = Registry::init(vec![d]).await?;
+    Ok(Registry::init(vec![d]).await?)
+}
+
+#[tokio::test]
+async fn test_load_registry() -> Result<(), Box<dyn std::error::Error>> {
+    let registry = load_test_registry().await?;
 
     assert_eq!(6, registry.objects.len());
     Ok(())
