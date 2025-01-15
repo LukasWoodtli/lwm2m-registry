788a19848bb315dfb76c8c99320a996a24f88f3b
Lukas Woodtli <lukas.woodtli@husqvarnagroup.com>
2025-01-15 12:04:05 +0100

    Add query for existance of object id

--- tests/registry.rs
+++ tests/registry.rs
@@ -1,4 +1,4 @@
-use lwm2m_registry::Registry;
+use lwm2m_registry::{Registry, Version};
 use std::path::PathBuf;
 
 async fn load_test_registry() -> Result<Registry, Box<dyn std::error::Error>> {
@@ -13,5 +13,13 @@ async fn test_load_registry() -> Result<(), Box<dyn std::error::Error>> {
     let registry = load_test_registry().await?;
 
     assert_eq!(6, registry.objects.len());
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_has_object_id() -> Result<(), Box<dyn std::error::Error>> {
+    let registry = load_test_registry().await?;
+    assert!(registry.has_object_id(1, Version::new(1, 1)));
+    assert!(!registry.has_object_id(1, Version::new(2, 1)));
     Ok(())
 }
